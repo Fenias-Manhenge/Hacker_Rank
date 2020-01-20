13ea6c7b531fd75065870d11bbc8953315e86b56
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-01-20 18:48:28 +0200

    Declaraçoes condicionais!

--- src/Solution.java
+++ src/Solution.java
@@ -17,27 +17,32 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("INTRODUZA UM NUMERO: ");
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         
-        if(N%2==1){
-            //impar
-        }
-        if(N%2==0) {
-            //par
-        }
-        for(int i = 2; i <= 100; i++){
-            if(1<=N && N<=100 && N>2 && N<5){
-                System.out.println("Not Weird");
-            }else{System.out.println("weird");}
-            
-            if(1<=N && N<=100 && N>6 && N<20){
+//        if(N%2==1){
+//            //impar
+//        }
+//        if(N%2==0) {
+//            //par
+//        }
+
+            if(N%2==1){
                 System.out.println("Weird");
             }
-            if(1<=N && N<=100 && 20>N){
+            
+            if(N%2==0 && 2<=N && N<=5){
                 System.out.println("Not Weird");
             }
-        }
+            
+            if(N%2==0 && 6<=N && N<=20){
+                System.out.println("Weird");
+            }
+            
+            if(N%2==0 && N > 20){
+                System.out.println("Not Weird");
+            }
         scanner.close();
     }
 }
